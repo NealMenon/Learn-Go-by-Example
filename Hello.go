4bc1dac0aa1bb13e71540a16912142a1d2aab6ec
nealmenon <nealmenon@gmail.com>
2020-05-04 15:11:36 +0530

    First

--- Hello.go
+++ Hello.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 )
 
+func helloer() string {
+	return "Hello you\n"
+}
+
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf(helloer())
 	var i int
 	i = 424
 	fmt.Println(i)
