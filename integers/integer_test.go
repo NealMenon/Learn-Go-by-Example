084b27f3f4de2c2161bd1a7fac5fdebe310a0576
nealmenon <nealmenon@gmail.com>
2020-05-05 23:37:04 +0530

    Integers complete

--- integers/integer_test.go
+++ integers/integer_test.go
@@ -1,12 +1,22 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Intn(100)
+	y := rand.Intn(100)
+	sum := Add(x, y)
+	expected := x + y
 
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	} else {
+		fmt.Printf("x = %d and y = %d\n", x, y)
 	}
 }
