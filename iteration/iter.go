03943c5e4d8462a61c82d067706f62d400070a0e
nealmenon <nealmenon@gmail.com>
2020-05-06 11:56:15 +0530

    Included mutiple tests for Repeat5 and RepeatN and benchmarks tests for both

--- iteration/iter.go
+++ iteration/iter.go
@@ -1,9 +1,21 @@
 package iteration
 
-// Repeat character n times
-func Repeat(character string, n int) string {
+// RepeatN character n times
+func RepeatN(character string, n int) string {
+	if n == 0 {
+		n = 5
+	}
 	var repeatedString string
 	for i := 0; i < n; i++ {
+		repeatedString += character
+	}
+	return repeatedString
+}
+
+// Repeat5 character 5 times
+func Repeat5(character string) string {
+	var repeatedString string
+	for i := 0; i < 5; i++ {
 		repeatedString += character
 	}
 	return repeatedString
--- iteration/iter_test.go
+++ iteration/iter_test.go
@@ -2,10 +2,37 @@ package iteration
 
 import "testing"
 
-func TestIter(t *testing.T) {
-	got := Repeat("a", 4)
-	want := "aaaa"
-	if got != want {
-		t.Errorf("Expected %q got %q", want, got)
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("Test Repeat5 for a", func(t *testing.T) {
+		got := Repeat5("a")
+		want := "aaaaa"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Test RepeatN for x, 7", func(t *testing.T) {
+		got := RepeatN("x", 7)
+		want := "xxxxxxx"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func BenchmarkRepeat5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat5("a")
+	}
+}
+
+func BenchmarkRepeatN(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RepeatN("x", 10)
 	}
 }
