4f574c1589c7631c70de58d28c0248a43f32fd11
nealmenon <nealmenon@gmail.com>
2020-05-07 09:20:31 +0530

    Update sum for slices, not array

--- arrays/sum.go
+++ arrays/sum.go
@@ -3,19 +3,29 @@ package main
 import "fmt"
 
 // Sum returns sum of array elems
-func Sum(numbers [5]int) int {
-	var sum int
-	// for i := 0; i < 5; i++ {
-	// 	sum += numbers[i]
-	// }
-	sum = 0
-	for _, val := range numbers {
+// func Sum(numbers [5]int) int {
+// 	var sum int
+// 	// for i := 0; i < 5; i++ {
+// 	// 	sum += numbers[i]
+// 	// }
+// 	sum = 0
+// 	for _, val := range numbers {
+// 		sum += val
+// 	}
+
+// 	return sum
+// }
+
+// Sum for slice
+func Sum(nums []int) int {
+	sum := 0
+	for _, val := range nums {
 		sum += val
 	}
-
 	return sum
 }
 
 func main() {
-	fmt.Println(Sum([5]int{1, 2, 3, 4, 5}))
+	// fmt.Println(Sum([]int{1, 2, 3, 4, 5}))
+	fmt.Println("Hi")
 }
