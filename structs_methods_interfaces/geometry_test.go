782f794c6990ee12f6fae456946257c007cd007c
nealmenon <nealmenon@gmail.com>
2020-05-07 14:54:36 +0530

    Complete tests and refactored

--- structs_methods_interfaces/geometry_test.go
+++ structs_methods_interfaces/geometry_test.go
@@ -20,18 +20,22 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name  string
 		shape Shape
-		want  float64
+		area  float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle_Area", shape: Rectangle{Width: 12, Height: 6}, area: 72.0},
+		{name: "Circle_Area", shape: Circle{Radius: 10}, area: 314.1592653589793},
+		{name: "Triangle_Area", shape: Triangle{Base: 12, Height: 6}, area: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.area {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.area)
+			}
+		})
+
 	}
 }
