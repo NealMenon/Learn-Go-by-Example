145770bda32b5140c2e274ebab1bf772fdb64958
nealmenon <nealmenon@gmail.com>
2020-05-08 02:49:33 +0530

    Introduce Bitcoin

--- pointersanderrors/fintech.go
+++ pointersanderrors/fintech.go
@@ -2,10 +2,21 @@ package main
 
 // Wallet struct
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
+// Bitcoin struct of type int
+type Bitcoin int
+
 // Balance in wallet
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+// Deposit amt into wallet
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
 
 func main() {
 
--- pointersanderrors/fintech_test.go
+++ pointersanderrors/fintech_test.go
@@ -5,10 +5,10 @@ import "testing"
 func TestWallet(t *testing.T) {
 	myWallet := Wallet{}
 
-	myWallet.Deposit(10)
+	myWallet.Deposit(Bitcoin(10))
 
 	got := myWallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("got %q expected %q", got, want)
