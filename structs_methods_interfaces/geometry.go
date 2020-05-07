2eca9e9c0a92450fdb1cd0f9908b94ab4dccf54e
nealmenon <nealmenon@gmail.com>
2020-05-07 14:20:19 +0530

    Methodds for ara of circle and rectangle

--- structs_methods_interfaces/geometry.go
+++ structs_methods_interfaces/geometry.go
@@ -1,9 +1,16 @@
 package main
 
+import "math"
+
 // Rectangle is shape with width and height
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+// Circle with radius
+type Circle struct {
+	Radius float64
 }
 
 // Perimeter returns perimeter of rectanlge in float64
@@ -12,8 +19,13 @@ func Perimeter(rect Rectangle) float64 {
 }
 
 // Area returns area of quadrilateral float64
-func Area(rect Rectangle) float64 {
-	return rect.Width * rect.Height
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+// Area of circle
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
 }
 
 func main() {
--- structs_methods_interfaces/geometry_test.go
+++ structs_methods_interfaces/geometry_test.go
@@ -2,22 +2,38 @@ package main
 
 import "testing"
 
-func TestGeometry(t *testing.T) {
-	recty := Rectangle{11.5, 12.5}
+func TestPerimeter(t *testing.T) {
 	checker := func(t *testing.T, got, want float64) {
 		t.Helper()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	}
-	t.Run("test_Perimeter()", func(t *testing.T) {
+	t.Run("rectangles_perimeter", func(t *testing.T) {
+		recty := Rectangle{11.5, 12.5}
 		got := Perimeter(recty)
 		want := 48.0
 		checker(t, got, want)
 	})
-	t.Run("test_Area()", func(t *testing.T) {
-		got := Area(recty)
+}
+
+func TestArea(t *testing.T) {
+	checker := func(t *testing.T, got, want float64) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+	t.Run("rectangles_area", func(t *testing.T) {
+		recty := Rectangle{11.5, 12.5}
+		got := recty.Area()
 		want := 143.75
+		checker(t, got, want)
+	})
+	t.Run("circles", func(t *testing.T) {
+		circy := Circle{10}
+		got := circy.Area()
+		want := 314.1592653589793
 		checker(t, got, want)
 	})
 }
