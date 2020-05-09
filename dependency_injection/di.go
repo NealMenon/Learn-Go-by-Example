bcb774c6254fcbd466664d566ce75f4c9335ec37
nealmenon <nealmenon@gmail.com>
2020-05-09 16:18:49 +0530

    Slight package fix

--- dependency_injection/di.go
+++ dependency_injection/di.go
@@ -3,19 +3,34 @@ package main
 import (
 	"fmt"
 	"io"
-	"net/http"
+	"os"
 )
 
-// Greet func
+// Greet (writer io.Writer, name string) {
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
-// MyGreeterHandler func
-func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
-	Greet(w, "world")
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
 
-func main() {
-	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
-}
+// package main
+
+// import (
+//     "fmt"
+//     "io"
+//     "net/http"
+// )
+
+// func Greet(writer io.Writer, name string) {
+//     fmt.Fprintf(writer, "Hello, %s", name)
+// }
+
+// func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+//     Greet(w, "world")
+// }
+
+// func main() {
+//     http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+// }
