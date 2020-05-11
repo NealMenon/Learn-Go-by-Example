849b3490db829e77ef280c3d787031bc88d2344e
nealmenon <nealmenon@gmail.com>
2020-05-11 11:54:52 +0530

    New struct result

--- concurrency/CheckWebsites.go
+++ concurrency/CheckWebsites.go
@@ -5,6 +5,12 @@ import "fmt"
 // WebsiteChecker func
 type WebsiteChecker func(string) bool
 
+// result is struct for each url
+type result struct {
+	string
+	bool
+}
+
 // CheckWebsites checks validity of website
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
