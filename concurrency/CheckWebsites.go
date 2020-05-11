789246b5a320aaf4a2fe42a45921bc9f93812ee7
nealmenon <nealmenon@gmail.com>
2020-05-11 12:00:54 +0530

    Now we can check websites faster, with channels

--- concurrency/CheckWebsites.go
+++ concurrency/CheckWebsites.go
@@ -14,12 +14,19 @@ type result struct {
 // CheckWebsites checks validity of website
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(url string) {
-			results[url] = wc(url)
+			// results[url] = wc(url)
+			resultChannel <- result{url, wc(url)}
 		}(url)
 
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
 	}
 
 	return results
