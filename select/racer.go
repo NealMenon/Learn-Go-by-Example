2b204ac1abc0aa75a839622b7879c8982430443e
nealmenon <nealmenon@gmail.com>
2020-05-12 14:40:40 +0530

    Refactor and make cleanups

--- select/racer.go
+++ select/racer.go
@@ -8,21 +8,20 @@ import (
 
 // Racer asdf
 func Racer(a, b string) string {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
-
 	return b
 }
 
+func measureResponseTime(u string) time.Duration {
+	start := time.Now()
+	http.Get(u)
+	return time.Since(start)
+}
 func main() {
 	fmt.Println("printer")
 }
