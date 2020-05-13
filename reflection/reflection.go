d7acd05af08f2b4e2df1eafc48c6ed8290c9c07d
nealmenon <nealmenon@gmail.com>
2020-05-13 09:25:38 +0530

    Essential test precode

---
+++ reflection/reflection.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/*
+ golang challenge: write a function
+ walk(x interface{}, fn func(string))
+ which takes a struct x and calls fn for
+ all strings fields found inside.
+ difficulty level: recursively.
+*/
+
+// Walk is the puzzle
+func Walk(x interface{}, fn func(string)) {
+
+}
+
+func main() {
+	fmt.Println("Build")
+}
