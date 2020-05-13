131b20a05f6db2ee6dddad6f6c0f7e8f584e7abc
nealmenon <nealmenon@gmail.com>
2020-05-14 02:54:26 +0530

    Sync test fails

--- sync/sync.go
+++ sync/sync.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// Counter is struct
+type Counter struct {
+	value int
+}
+
+// Inc increments counter value
+func (c *Counter) Inc() {
+	c.value++
+}
+
+// Value increments counter value
+func (c *Counter) Value() int {
+	return c.value
+}
+
 func main() {
 	fmt.Println("Build")
 }
