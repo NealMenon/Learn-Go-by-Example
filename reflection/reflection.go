e28d545b0c67dc9906ccdee37aec202828f1f414
nealmenon <nealmenon@gmail.com>
2020-05-13 10:18:20 +0530

    init build

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -15,6 +15,10 @@ import (
 func walk(x interface{}, fn func(string)) {
 	val := reflect.ValueOf(x)
 
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
